6e5109d1d18c7e6aadfdbd9ec8aa74f53ec2cdd0
datoona <davit.arzumanyan92@gmail.com>
2018-10-22 21:25:49 +0400

    success login pass

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
@@ -12,10 +13,14 @@ import static org.testng.Assert.assertTrue;
 public class LoginTest {
     private WebDriver driver;
 
-    @Test
-    public void successLogin() {
+    @BeforeMethod
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Browser Driver\\chromedriver.exe");
         driver = new ChromeDriver();
+    }
+
+    @Test
+    public void successLogin() {
         driver.get("https://www.google.com");
         assertTrue(driver.getCurrentUrl().contains("https://www.google.com"), "The page was not load correctly");
 
@@ -24,14 +29,11 @@ public class LoginTest {
 
     @Test
     public void githubFailedLogin() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Browser Driver\\chromedriver.exe");
-        driver = new ChromeDriver();
         driver.get("https://www.github.com/login");
-
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.loginWith("test@test.com","test");
-        
-        assertTrue(loginPage.isErrorMessageDisplayed(),"Error message is displayed");
+        loginPage.loginWith("test@test.com", "test");
+
+        assertTrue(loginPage.isErrorMessageDisplayed(), "Error message is displayed");
     }
 
     @AfterMethod
