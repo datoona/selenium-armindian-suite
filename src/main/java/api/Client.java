5ae944adfa4a3ece3182bf41fb6cc3963692b6ec
datoona <davit.arzumanyan92@gmail.com>
2018-11-12 21:28:18 +0400

    add api call

---
+++ src/main/java/api/Client.java
@@ -0,0 +1,49 @@
+package api;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String accessToken = getAccessToken();
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"is_private\":false,\"creation_template\":1,\"description\":\"testik\",\"name\":\"testik\"}");
+        Request request = new Request.Builder()
+                .url("https://api.taiga.io/api/v1/projects")
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .addHeader("Cache-Control", "no-cache")
+                .addHeader("Postman-Token", "95476f30-9e9b-b10f-22ba-700e50dd22e9")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+
+    }
+
+    public static String getAccessToken() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"username\":\"davetest@mailinator.com\",\"password\":\"davetest\",\"type\":\"normal\"}");
+        Request request = new Request.Builder()
+                .url("https://api.taiga.io/api/v1/auth")
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Cache-Control", "no-cache")
+                .addHeader("Postman-Token", "3e2fe44e-ebbc-ffdc-ed6e-8c9637bb3608")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String jsonString = response.body().string();
+        JsonObject jsonObject = new JsonParser().parse(jsonString).getAsJsonObject();
+        return jsonObject.get("auth_token").getAsString();
+    }
+
+}
