ba4c8b6923ae4353208bdb0ecf9f53196ea6c4de
datoona <davit.arzumanyan92@gmail.com>
2018-11-19 20:34:47 +0400

    please observe login api

--- src/test/java/SeleniumBase.java
+++ src/test/java/SeleniumBase.java
@@ -1,11 +1,23 @@
+import api.Client;
 import base.DriverHelper;
+import com.google.gson.JsonObject;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.AfterMethod;
 
+import java.io.IOException;
 
 
 public class SeleniumBase {
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         DriverHelper.get().quitDriver(DriverHelper.get().getDriver());
+    }
+
+    //davetest@mailinator.com", "davetest"
+    public void login(String email, String password) throws IOException {
+        JsonObject loginJson = Client.login(email, password);
+        JavascriptExecutor jse = (JavascriptExecutor) DriverHelper.get().getDriver();
+        jse.executeScript("window.localStorage.setItem('token', '" + loginJson.get("auth_token") + "');");
+        jse.executeScript("window.localStorage.setItem('userInfo', '" + loginJson + "');");
     }
 }
--- src/test/java/TaigaLoginTest.java
+++ src/test/java/TaigaLoginTest.java
@@ -1,7 +1,3 @@
-import api.Client;
-import base.DriverHelper;
-import com.google.gson.JsonObject;
-import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -10,11 +6,7 @@ public class TaigaLoginTest extends SeleniumBase {
     @Test
     public void loginViaApi() throws IOException {
         TaigaLoginPage taigaLoginPage = new TaigaLoginPage();
-        JsonObject loginJson = Client.login("davetest@mailinator.com", "davetest");
-        JavascriptExecutor jse = (JavascriptExecutor) DriverHelper.get().getDriver();
-        jse.executeScript("window.localStorage.setItem('token', '" + loginJson.get("auth_token") + "');");
-        jse.executeScript("window.localStorage.setItem('userInfo', '" + loginJson + "');");
-
+        login("davetest@mailinator.com","davetest");
         new TaigaLoginPage();
 
     }
